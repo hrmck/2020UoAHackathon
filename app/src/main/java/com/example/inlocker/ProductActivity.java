0082af94bc798fc354cc099304da31474d130108
hrmck <agnesleunghc@gmail.com>
2020-05-05 02:54:22 +0900

    activity_products.xml  - a bunch of different layouts -> constraintLayout

--- app/src/main/java/com/example/inlocker/ProductActivity.java
+++ app/src/main/java/com/example/inlocker/ProductActivity.java
@@ -1,6 +1,9 @@
 package com.example.inlocker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,17 +13,26 @@ public class ProductActivity extends AppCompatActivity {
 
     TextView storeName;
     String[] categories;
+    ImageButton cart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
 
-        storeName = findViewById(R.id.store_name);
+        storeName = findViewById(R.id.storeName);
         String received_storeName = getIntent().getStringExtra("chosenStoreName");
         storeName.setText(received_storeName);
 
-        categories = getResources().getStringArray(R.array.categories);
+        //categories = getResources().getStringArray(R.array.categories);
 
+        cart = (ImageButton) findViewById(R.id.cartBtn);
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProductActivity.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
