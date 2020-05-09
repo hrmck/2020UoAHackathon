1a64a3ba5b9b89566788c9c9dbf3305d37574957
Internet-today <61218439+Internet-today@users.noreply.github.com>
2020-05-09 13:51:39 +0900

    This time I connected the code to firestone. I wrote the code to try and retrieve the products list... The code is below the commented code on firebase in the productactivity.java file
    Write now i am trying to see what display is fetched from the database...

--- app/src/main/java/com/example/inlocker/ProductActivity.java
+++ app/src/main/java/com/example/inlocker/ProductActivity.java
@@ -2,6 +2,7 @@ package com.example.inlocker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -13,11 +14,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
@@ -54,7 +61,7 @@ public class ProductActivity extends AppCompatActivity {
         b = (TextView) findViewById(R.id.b);
         c = (TextView) findViewById(R.id.c);
 
-        DrinksBtn.setOnClickListener(new View.OnClickListener() {
+        /*DrinksBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //getting the end branch we want to search
@@ -78,6 +85,32 @@ public class ProductActivity extends AppCompatActivity {
 
                     }
                 });
+            }
+        });
+*/
+
+        //for using firestore
+        DrinksBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference Storeref = db.document("storeList/5bineW8oGWA3vRRj1XOY/Products");
+                //db.collection("storeList");
+                Storeref.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    //String info = documentSnapshot.getCollection
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                    }
+                                } else {
+                                    Log.w(TAG, "Error getting documents.", task.getException());
+                                }
+                            }
+                        });
             }
         });
 
