1b93c4224f1d56b4e89d404c6bff3c95f589591d
hrmck <agnesleunghc@gmail.com>
2020-05-13 23:50:58 +0900

    added User Authentication

--- app/src/main/java/com/example/inlocker/LoginActivity.java
+++ app/src/main/java/com/example/inlocker/LoginActivity.java
@@ -2,28 +2,93 @@ package com.example.inlocker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.Objects;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Button login_btn;
+    EditText editTextEmail, editTextPwd;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        login_btn = findViewById(R.id.LoginBtn_main);
 
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toMain = new Intent(LoginActivity.this, StoreActivity.class);
-                startActivity(toMain);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.login_progressBar);
+        editTextEmail = findViewById(R.id.EmailEditText);
+        editTextPwd = findViewById(R.id.PasswordEditText);
+
+        login_btn = findViewById(R.id.LoginBtn_main);
+        login_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.LoginBtn_main) {
+            userLogin();
+        }
+        ;
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String pwd = editTextPwd.getText().toString().trim();
+
+        if (isValidLoginCred(email, pwd)) {
+            progressBar.setVisibility(View.VISIBLE);
+            mAuth.signInWithEmailAndPassword(email, pwd)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                Intent toMenu = new Intent(LoginActivity.this, StoreActivity.class);
+                                toMenu.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(toMenu);
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private Boolean isValidLoginCred(String email, String pwd) {
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return false;
+        }
+        if (pwd.isEmpty()) {
+            editTextPwd.setError("Password is required");
+            editTextPwd.requestFocus();
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/inlocker/SignUpActivity.java
+++ app/src/main/java/com/example/inlocker/SignUpActivity.java
@@ -1,15 +1,119 @@
 package com.example.inlocker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.Objects;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    Button signUp;
+    EditText editTextEmail, editTextPwd, editTextPwdC;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.signUp_emailAdr);
+        editTextPwd = findViewById(R.id.signUp_pwd);
+        editTextPwdC = findViewById(R.id.signUp_pwd_c);
+
+        progressBar = findViewById(R.id.signUp_progressBar);
+
+        signUp = findViewById(R.id.signUpBtn);
+        signUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.signUpBtn) {
+            registerUser();
+        }
+        ;
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String pwd = editTextPwd.getText().toString().trim();
+        String pwdC = editTextPwdC.getText().toString().trim();
+
+        if (isValidSignUpCred(email, pwd, pwdC)) {
+            progressBar.setVisibility(View.VISIBLE);
+            mAuth.createUserWithEmailAndPassword(email, pwd)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                Intent toMain = new Intent(SignUpActivity.this, MainActivity.class);
+                                toMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(toMain);
+                            } else {
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "You're already registered", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(),
+                                            Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+
+    private boolean isValidSignUpCred(String email, String pwd, String pwdC) {
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return false;
+        }
+        if (pwd.isEmpty()) {
+            editTextPwd.setError("Password is required");
+            editTextPwd.requestFocus();
+            return false;
+        }
+        if (pwdC.isEmpty()) {
+            editTextPwdC.setError("Confirm password is required");
+            editTextPwdC.requestFocus();
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return false;
+        }
+        if (pwd.length() < 6) {
+            editTextPwd.setError("Minimum length of password should be 6");
+            editTextPwd.requestFocus();
+            return false;
+        }
+        if (!pwd.equals(pwdC)) {
+            //not same password, error
+            editTextPwdC.setError("Both passwords are not the same");
+            editTextPwdC.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
