175a946f4fbf72cc959eb58894c18a987b36ffc9
Internet-today <61218439+Internet-today@users.noreply.github.com>
2020-05-15 14:51:57 +0900

    testing out the new page. debugging the red line.

--- app/src/main/java/com/example/inlocker/SignUpActivity.java
+++ app/src/main/java/com/example/inlocker/SignUpActivity.java
@@ -65,8 +65,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             progressBar.setVisibility(View.GONE);
                             if (task.isSuccessful()) {
-                                Intent toMain = new Intent(SignUpActivity.this, MainActivity.class);
-                                toMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                Intent toMain = new Intent(SignUpActivity.this, shopperDetails.class);
+                                //toMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                 startActivity(toMain);
                             } else {
                                 if (task.getException() instanceof FirebaseAuthUserCollisionException) {
